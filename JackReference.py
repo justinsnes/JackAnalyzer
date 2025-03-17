ec353013be8556c15b57d66202925f9f84e390d9
Justin Elliott <justin@medialabinc.com>
2025-03-17 03:38:46 -0400

    Complete keywords and symbols reference

--- JackReference.py
+++ JackReference.py
@@ -4,9 +4,9 @@ class JackReference:
     def __init__(self):
         pass
 
-    keywords = ["boolean", "char", "class", "constructor", "do", "else", "field", "function", "if", "int", "let", "method", "return", "static",
-                "this", "var", "void", "while"]
-    symbols = ["{", "}", "(", ")", "[", "]", ";", ",", ".", "+", "-", "/", "=", "<", ">"]
+    keywords = ["boolean", "char", "class", "constructor", "do", "else", "false", "field", "function", "if", "int", "let", "method", "null",
+                "return", "static", "this", "true", "var", "void", "while"]
+    symbols = ["{", "}", "(", ")", "[", "]", ";", ",", ".", "+", "-", "*", "/", "=", "<", ">", "|", "&", "~"]
     # get list of elements per line
     regexSymbols = "\\" + "|\\".join(symbols)
     regexPattern = re.compile(r'\w+|' + regexSymbols + '|".+"')
